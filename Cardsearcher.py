3ce8341a77ec465c99492a435a2ec78787d4a93c
Shaco216 <p.griffin.1911@web.de>
2022-01-04 00:23:27 +0100

    solved problem with cardname - it was 1 character too long but the character was not visible - solved by line 18

--- Cardsearcher.py
+++ Cardsearcher.py
@@ -5,9 +5,22 @@ cardname = ""
 
 def scrape_for_card(cardname):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    cardname = '["' + cardname + '"]'
-    link = "https://www.cardmarket.com/de/Magic/Products/Search?searchString=" + cardname
+
+    #to find exactly that card you need to use ["name"] in the searchbar of the website
+    #but the link will change [ to %5B and ] to %5D ""stay the same on addressbar but not in python ( see example )
+    #space will be a +
+    #example https://www.cardmarket.com/en/Magic/Products/Search?searchString=%5B%22sliverqueen%5B%22
+
+    #cardname = '%5B"'+cardname+'"%5D'
+
+    #probably the cardname is not correct because if i use a copyied link like this one https://www.cardmarket.com/de/Magic/Products/Search?searchString=sliverqueen it is working
+    #as i thought the lenght of cardname is 1 character to long
+    cardname = cardname[0:len(cardname)-1]
+    #problem solved with line 18
+
+    link = "https://www.cardmarket.com/de/Magic/Products/Search?searchString="+str(cardname)
     print(link)
+    link = str(link)
     req = requests.get(link, headers=headers)
     soup = BeautifulSoup(req.content, "html.parser")
     print(soup)
